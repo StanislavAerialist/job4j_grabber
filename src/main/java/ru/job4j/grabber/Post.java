f69bdd8756f2ad502000877eba2862a7676fb795
StanislavAerialist <moonlight_stas@mail.ru>
2022-10-27 20:39:12 +0500

    2.2. Модель данных - Post. [#285211 #355223]

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -11,6 +11,21 @@ public class Post {
     private String description;
     private LocalDateTime created;
 
+    public Post(String title, String link, String description, LocalDateTime created) {
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
+
+    public Post(int id, String title, String link, String description, LocalDateTime created) {
+        this.id = id;
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
+
     public int getId() {
         return id;
     }
@@ -51,21 +66,6 @@ public class Post {
         this.created = created;
     }
 
-    public Post(String title, String link, String description, LocalDateTime created) {
-        this.title = title;
-        this.link = link;
-        this.description = description;
-        this.created = created;
-    }
-
-    public Post(int id, String title, String link, String description, LocalDateTime created) {
-        this.id = id;
-        this.title = title;
-        this.link = link;
-        this.description = description;
-        this.created = created;
-    }
-
     @Override
     public String toString() {
         return "Post{"
@@ -85,13 +85,11 @@ public class Post {
             return false;
         }
         Post post = (Post) o;
-        return id == post.id
-                && Objects.equals(title, post.title)
-                && Objects.equals(link, post.link);
+        return id == post.id && Objects.equals(link, post.link);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, title, link);
+        return Objects.hash(id, link);
     }
 }
